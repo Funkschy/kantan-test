f648e7375f4f6bf555527b01f8f71c32ebee5624
Funkschy <felix.schoeller@protonmail.com>
2020-02-02 16:07:55 +0100

    added kantan-test-ignore
    filenames in this file will be ignored

--- src/read.py
+++ src/read.py
@@ -1,10 +1,25 @@
-from typing import List
+from typing import List, Set
 from os import listdir
-from os.path import abspath, join, splitext
+from os.path import abspath, join, splitext, isfile, basename
 
 from testcase import TestCase
 
 
 def read(test_path: str) -> List[TestCase]:
-    files = filter(lambda f: splitext(f)[1] == '.py', [abspath(join(test_path, f)) for f in listdir(test_path)])
+    ignored = read_ignored(test_path)
+
+    def add_file(f) -> bool:
+        st = splitext(f)
+        return st[1] == '.py' and basename(st[0]) not in ignored
+
+    files = filter(add_file, [abspath(join(test_path, f)) for f in listdir(test_path)])
     return [TestCase(path) for path in files]
+
+
+def read_ignored(test_path: str) -> Set[str]:
+    ignore_file = join(test_path, 'kantan-test-ignore')
+    if isfile(ignore_file):
+        with open(ignore_file) as f:
+            return set(map(lambda n: n.strip(), f.readlines()))
+
+    return set()
